78095546feca24ae93f67c410703fc659fc76689
leennnS <leensamady123@gmail.com>
2026-03-15 22:56:47 +0200

    added MarketScene interior/exterior

---
+++ Assets/Scripts/Market/MarketSceneSetup.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class MarketSceneSetup : MonoBehaviour
+{
+    [SerializeField] private Transform marketSpawnPoint;
+    [SerializeField] private CinemachineCamera cinemachineCamera;
+
+    private void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            if (marketSpawnPoint != null)
+                player.transform.position = marketSpawnPoint.position;
+
+            if (cinemachineCamera != null)
+                cinemachineCamera.Target.TrackingTarget = player.transform;
+        }
+        else
+        {
+            Debug.LogWarning("Player not found in Market scene.");
+        }
+    }
+}
